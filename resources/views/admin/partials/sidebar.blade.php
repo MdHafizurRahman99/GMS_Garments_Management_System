70dba0883dac40a230e51952b48f8ef510718342
hfvector <hfhafiz0@gmail.com>
2025-05-11 23:14:07 +0600

    Update sidebar to display different staff information links for admin and non-admin users

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -33,12 +33,13 @@
     @if (Auth::user()->can('staff.view'))
         <hr class="sidebar-divider my-0">
         <li class="nav-item">
+
+            @if (auth()->user()->role == 'admin')
             <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseStaff"
                 aria-expanded="true" aria-controls="collapseStaff">
                 {{-- <i class="fa fa-Staff" aria-hidden="true"></i> --}}
                 <span>Staff informations</span>
             </a>
-
             <div id="collapseStaff" class="collapse" aria-labelledby="headingStaff" data-parent="#accordionSidebar">
                 <div class="bg-white py-2 collapse-inner rounded">
                     {{-- <h6 class="collapse-header">Custom Components:</h6> --}}
@@ -47,8 +48,26 @@
                     {{-- <a class="collapse-item" href="cards.html">Cards</a> --}}
                 </div>
             </div>
+            @else
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseStaff"
+                    aria-expanded="true" aria-controls="collapseStaff">
+                    {{-- <i class="fa fa-Staff" aria-hidden="true"></i> --}}
+                    <span>Additional informations</span>
+                </a>
+                <div id="collapseStaff" class="collapse" aria-labelledby="headingStaff" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        {{-- <h6 class="collapse-header">Custom Components:</h6> --}}
+                        {{-- <a class="collapse-item" href="{{ route('staff.create') }}">Staff Form</a> --}}
+                        <a class="collapse-item" href="{{ route('staff.create') }}">Create/Update</a>
+                        {{-- <a class="collapse-item" href="cards.html">Cards</a> --}}
+                    </div>
+                </div>
+            @endif
+
+
         </li>
     @endif
+
     <!-- Divider -->
     @if (Auth::user()->can('shift.view'))
         <li class="nav-item">
